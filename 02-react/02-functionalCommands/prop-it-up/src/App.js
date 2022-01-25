51f621506ee61000424f7b87955ae1b2310efe92
neomarwilliams <neoma.rae@gmail.com>
2022-01-24 16:37:37 -0800

    putting it all together

--- 02-react/02-functionalCommands/prop-it-up/src/App.js
+++ 02-react/02-functionalCommands/prop-it-up/src/App.js
@@ -7,25 +7,25 @@ function App() {
       <PersonCard  
         firstName = { "Jane" } 
         lastName = { "Doe" } 
-        age = { 45 }
+        initialAge = { 45 }
         hairColor = { "Black" }
       />
       <PersonCard  
         firstName = { "John" } 
         lastName = { "Smith" } 
-        age = { 88 }
+        initialAge = { 88 }
         hairColor = { "Brown" }
       />
       <PersonCard  
         firstName = { "Millard" } 
         lastName = { "Fillmore" } 
-        age = { 50 }
+        initialAge = { 50 }
         hairColor = { "Brown" }
       />
       <PersonCard  
         firstName = { "Maria" } 
         lastName = { "Smith" } 
-        age = { 62 }
+        initialAge = { 62 }
         hairColor = { "Brown" }
       />
     </div>
--- 02-react/02-functionalCommands/prop-it-up/src/components/PersonCard.js
+++ 02-react/02-functionalCommands/prop-it-up/src/components/PersonCard.js
@@ -1,12 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const PersonCard = (props) => {
-    const { firstName, lastName, age, hairColor } = props;
+    const { firstName, lastName, hairColor, initialAge } = props;
+    const [age, setAge] = useState(initialAge);
     return (
         <div>
             <h1>{ lastName }, { firstName }</h1>
             <p>Age: { age }</p>
             <p>Hair Color: { hairColor }</p>
+            <button onClick= { (event) => setAge (age + 1)}> { firstName } had a birthday!</button>
         </div>
     )
 }
